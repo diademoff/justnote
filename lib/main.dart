7b8ca1abfcf88d54840293f4793d09ca3b67da30
diademoff <diademoff@yandex.ru>
2021-04-22 17:36:39 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  @override
+  _App createState() => _App();
+}
+
+class _App extends State<App> {
+  bool isTextHidden = true;
+  TextEditingController textFieldController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: getAppBar(),
+        body: Container(
+          alignment: Alignment.topCenter,
+          constraints: BoxConstraints.expand(),
+          child: TextField(
+            autofocus: true,
+            style: TextStyle(fontSize: 18.0, color: getTextColor(isTextHidden)),
+            maxLines: null,
+            keyboardType: TextInputType.multiline,
+            controller: textFieldController,
+          ),
+        ),
+      ),
+    );
+  }
+
+  AppBar getAppBar() {
+    return AppBar(
+      leading: IconButton(
+        icon: Icon(Icons.add_a_photo),
+        onPressed: () {
+          setState(() {
+            isTextHidden = !isTextHidden;
+          });
+        },
+      ),
+      title: Text("Just Note"),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.accessible_forward),
+          onPressed: () {
+            setState(() {
+              addNumberToTextField();
+            });
+          },
+        )
+      ],
+    );
+  }
+
+  Color getTextColor(bool isTextHidden) {
+    if (isTextHidden) {
+      return Colors.white;
+    } else {
+      return Colors.black87;
+    }
+  }
+
+  void addNumberToTextField() {
+
+  }
+}
