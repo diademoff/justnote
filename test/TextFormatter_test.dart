b1ba84ca027ed07a13091a2c5e69a06aadcce46a
diademoff <diademoff@yandex.ru>
2021-04-23 07:06:55 +0300

    add tests

--- test/TextFormatter_test.dart
+++ test/TextFormatter_test.dart
@@ -2,9 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:justnote/TextFormatter.dart';
 
 void main() {
-  test('TextFormatter test', () async {
+  test('TextFormatter remove blank lines test', () async {
     var tests = [
-      
       ["\n\t\t\n\t", ""],
       ["\n\t\t\n\t ", " "],
       ["\n\t \t\n\t", " "],
@@ -13,6 +12,9 @@ void main() {
       ["test1\t test2\t tst\n", "test1\t test2\t tst"],
       ["  ", " "],
       ["                   \n", " "],
+      ["Paragraph\nnew line \n \n \t \n", "Paragraph\nnew line "],
+      ["1. line\n2.line\nline\n", "1. line\n2.line\nline"],
+      ["1. line\n2. line", "1. line\n2. line"]
     ];
 
     for (var test in tests) {
@@ -21,6 +23,30 @@ void main() {
 
       var formatter = TextFormatter(input);
       formatter.removeWhiteLines();
+
+      expect(formatter.formattedText, outputExpected);
+    }
+  });
+
+  test('TextFormatter remove blank and add next number', () async {
+    var tests = [
+      ["", "1. "],
+      ["98. line", "98. line\n99. "],
+      ["98. line\n99. ", "98. line\n99. "],
+      ["98. line\n99. new line", "98. line\n99. new line\n100. "],
+      ["1. first", "1. first\n2. "],
+      ["1. first\n2. second\nsecond too", "1. first\n2. second\nsecond too\n3. "]
+    ];
+
+    for (var test in tests) {
+      var input = test[0];
+      var outputExpected = test[1];
+
+      var formatter = TextFormatter(input);
+      formatter.removeWhiteLines();
+      if (!formatter.isNumberAdded()) {
+        formatter.addNumber();
+      }
 
       expect(formatter.formattedText, outputExpected);
     }
